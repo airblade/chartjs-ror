airblade/chartjs-ror

commit 44a9c31b00ae328300f33fe8a60ca2e2dc00ccd6

Do not modify options.
Closes #40.

lib/chartjs/chart_helpers.rb
@@ -27,11 +27,13 @@ module Chartjs
     private
 
     def chart(type, data, options)
+      opts = options.dup
+
       @chart_id ||= -1
-      element_id = options.delete(:id)     || "chart-#{@chart_id += 1}"
-      css_class  = options.delete(:class)  || 'chart'
-      width      = options.delete(:width)  || '400'
-      height     = options.delete(:height) || '400'
+      element_id = opts.delete(:id)     || "chart-#{@chart_id += 1}"
+      css_class  = opts.delete(:class)  || 'chart'
+      width      = opts.delete(:width)  || '400'
+      height     = opts.delete(:height) || '400'
 
       canvas = content_tag :canvas, '', id: element_id, class: css_class, width: width, height: height
 
@@ -43,7 +45,7 @@ module Chartjs
             var chart = new Chart(ctx, {
               type:    "#{camel_case type}",
               data:    #{to_javascript_string data},
-              options: #{to_javascript_string options}
+              opts: #{to_javascript_string opts}
             });
           };
 

commit d0a0f595301434052cd534947fd229e48c248bd7

fix passing Chart.js options
The bug was introduced in https://github.com/airblade/chartjs-ror/commit/44a9c31b00ae328300f33fe8a60ca2e2dc00ccd6 and hasn't been released yet.

lib/chartjs/chart_helpers.rb
@@ -45,7 +45,7 @@ module Chartjs
             var chart = new Chart(ctx, {
               type:    "#{camel_case type}",
               data:    #{to_javascript_string data},
-              opts: #{to_javascript_string opts}
+              options: #{to_javascript_string opts}
             });
           };
 