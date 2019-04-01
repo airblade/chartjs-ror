383892808e4934235aec958fd534c311c16679f6
evs-ch <evs-ch@users.noreply.github.com>
2019-04-01 14:08:53 +0200

    provide option to disable css injection

--- lib/chartjs.rb
+++ lib/chartjs.rb
@@ -1,5 +1,6 @@
 module Chartjs
   @@no_conflict = false
+  @@disable_css_injection = false
 
   def self.no_conflict!
     @@no_conflict = true
@@ -7,6 +8,14 @@ module Chartjs
 
   def self.no_conflict
     @@no_conflict
+  end
+
+  def self.disable_css_injection!
+    @@disable_css_injection = true
+  end
+
+  def self.disable_css_injection
+    @@disable_css_injection
   end
 end
 
